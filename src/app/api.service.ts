ab6e47e9fb7666760aea503db91a4b3eaaad6c32
Przemysław Pyk <pyk.przemyslaw@gmail.com>
2020-05-07 16:26:13 +0200

    Initialize

---
+++ src/app/api.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders
+} from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  constructor(private http: HttpClient) {}
+  // Service to get data
+  getWords(word: string) {
+    const _headers = new HttpHeaders();
+    const headers: HttpHeaders = _headers
+      .append('Accept', 'application/json')
+      .append('app_id', 'cb35ef20')
+      .append('app_key', '603be016c344650e6d8c1da15330f7af');
+    return this.http.get(`/api/search/en?q=${word}&prefix=false&limit=10`, {
+      headers
+    });
+  }
+  // Service to get full data
+  getDefinition(word: string) {
+    const _headers = new HttpHeaders();
+    const headers: HttpHeaders = _headers
+      .append('Accept', 'application/json')
+      .append('app_id', 'cb35ef20')
+      .append('app_key', '603be016c344650e6d8c1da15330f7af');
+    return this.http.get(`/api/entries/en/${word}`, { headers });
+  }
+  // Service to get synonyms and antonyms
+  getSynAnt(word: string) {
+    const _headers = new HttpHeaders();
+    const headers: HttpHeaders = _headers
+      .append('Accept', 'application/json')
+      .append('app_id', 'cb35ef20')
+      .append('app_key', '603be016c344650e6d8c1da15330f7af');
+    return this.http.get(`/api/entries/en/${word}/synonyms;antonyms`, {
+      headers
+    });
+  }
+}
---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { FullViewComponent } from './full-view/full-view.component';
+import { SearchComponent } from './search/search.component';
+import { AddComponent } from './add/add.component';
+const routes: Routes = [
+  { path: 'search', component: SearchComponent },
+  { path: 'add', component: AddComponent },
+  { path: 'definition/:id', component: FullViewComponent },
+  { path: '', redirectTo: '/search', pathMatch: 'full' },
+  { path: '**', component: SearchComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
